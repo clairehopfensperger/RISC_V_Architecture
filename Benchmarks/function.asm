b315ca696be85fc7ee535178ac84658dcfab2fad
Claire Hopfensperger <99447241+clairehopfensperger@users.noreply.github.com>
2023-04-02 13:50:04 -0400

    created benchmarks folder with function.asm

---
+++ Benchmarks/function.asm
@@ -0,0 +1,20 @@
+# function
+	# recursive factorial
+
+main1:		addi t0, zero, 5  # factorial = 5
+		addi t1, zero, 4  # num = 4
+		
+		jal FUNC_CALL
+		jal exit
+		
+FUNC_CALL:	blt zero, t1, IF_BODY  # if (0 < num)
+		jalr ra
+		
+IF_BODY:	mul t0, t0, t1  # factorial *= num
+		addi t1, t1, -1  # num -= 1
+		jal FUNC_BODY
+		
+FUNC_BODY:	jal FUNC_CALL
+
+exit:		
+	
