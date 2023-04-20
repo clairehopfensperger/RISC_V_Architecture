c0186aa54a3140a4081be116ca289eeb07589ae0
Claire Hopfensperger <99447241+clairehopfensperger@users.noreply.github.com>
2023-04-20 12:00:29 -0400

    uploaded testing.asm

---
+++ Benchmarks/testing.asm
@@ -0,0 +1,32 @@
+addi t0, zero, 2
+addi t1, zero, 4
+addi t2, zero, 6
+addi t3, zero, -5
+addi t4, zero, 3
+addi t5, zero, -6
+addi t6, zero, 1
+addi s11, zero, 7
+
+# r-type
+#add s0, t0, t1
+#sub s1, t0, t1
+#mul s2, t0, t1
+#and s3, t0, t1
+#or s4, t0, t2
+#xor s5, t0, t2
+#slt s6, t3, t4 
+#sltu s7, t3, t4
+#sra s8, t5, t6
+#srl s9, t4, t6
+#sll s10, t4, t6
+
+# i-type
+#addi s0, s11, 3
+#andi s1, t1, 2
+#ori s2, t2, 2
+#xori s3, t2, 2
+#slti s4, t3, 3
+#sltiu s5, t3, 3
+#rai s6, t5, 1
+#srli s7, t4, 1
+#slli s8, t4, 1
