be9fa26f8a8981e8a3787d02ec09ecf761176d69
Claire Hopfensperger <99447241+clairehopfensperger@users.noreply.github.com>
2023-04-21 08:49:40 -0400

    created Register.v (not tested)

---
+++ Verilog/Register/Register.v
@@ -0,0 +1,22 @@
+module Register{
+	input clk, rst,
+	input [31:0]in,
+	input en,
+	output reg [31:0]out
+};
+
+always @(posedge clk or negedge rst)
+begin
+
+	if (rst == 1'b0)
+	begin
+		out <= 32'd0;
+	end
+	else if (en == 1'b1)
+	begin
+		out <= in;
+	end
+
+end
+
+endmodule
